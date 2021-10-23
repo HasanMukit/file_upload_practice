de6a196548d536b26fc7881e6b39e10a9412ce1f
Mukit Hasan <mukit.hasan07@gmail.com>
2021-10-23 15:07:33 +0600

    initial commit for heroku test

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const multer = require('multer')
+const ejs = require('ejs')
+const path = require('path')
+
+// Set Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+})
+// Init Upload 
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 5000000},
+    fileFilter: function(req, file, cb) {
+        checkFileType(file,cb)
+    }
+}).single('Docs')
+//Check File Type
+function checkFileType(file, cb) {
+    //Allowed ext
+    const filetypes = /pdf|doc|docx/
+    //Check ext 
+    const extname = filetypes.test(path.extname(file.originalname).toLocaleLowerCase())
+    //Check mime
+    const mimetype = filetypes.test(file.mimetype)
+    if(mimetype && extname) {
+        return cb(null, true)
+    }
+    cb('Error: Pdf/Doc File only!')
+}
+
+//Init app
+const app = express()
+
+//EJS
+app.set('view engine', 'ejs');
+
+//Public Folder 
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'))
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err) {
+            res.render('index', {
+                msg: err
+            })
+        }
+        else {
+            if(req.file == undefined) {
+                res.render('index', {
+                    msg: 'Error: No File Selected'
+                })
+            }
+            else {
+                res.render('index', {
+                    msg: 'File Uploaded!',
+                    file: `uploads/${req.file.filename}`
+                })
+            }
+        }
+    })
+})
+const port = 3000;
+app.listen(port, () => console.log(`server started ib port ${port}`))
